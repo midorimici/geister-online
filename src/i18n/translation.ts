9bd0374c07750c7c01bb14b8e0b842c2b0f2ba92
mici <60635066+midorimici@users.noreply.github.com>
2021-12-28 23:54:16 +0900

    🌐 Update language judgement code

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -1,7 +1,11 @@
 import dict from './dict';
 
 /** Whether the language is English. */
-const isEN: boolean = location.pathname.includes('/en/');
+const isEN: boolean =
+  document.cookie
+    ?.split('; ')
+    .find((row: string) => row.startsWith('firebase-language-override'))
+    ?.split('=')[1] !== 'ja';
 
 export const t = (key: keyof typeof dict, arg?: any): string => {
   let value = dict[key][isEN ? 'en' : 'ja'];
